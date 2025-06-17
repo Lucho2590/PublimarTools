5c7d383f6ff3fb21d1ed7b736250a79777dba173
Lucho2590 <lopezlucianomartin@gmail.com>
2025-06-17 17:49:01 -0300

    Actualizado

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2KDer0QebLKNQBKL4KG917mcX4iwnU8o",
-  authDomain: "publimartools.firebaseapp.com",
-  projectId: "publimartools",
-  storageBucket: "publimartools.firebasestorage.app",
-  messagingSenderId: "202477271278",
-  appId: "1:202477271278:web:f0aabd5fb12a900fdfdbff",
-  measurementId: "G-N64NKFM7PX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
